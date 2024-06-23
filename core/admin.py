ba1866b8dfd082db217a0f1a6ce9819502ea1507
gleydes <gley_dez-10@hotmail.com>
2024-06-23 00:13:33 -0300

    Segundo commit alteração do banco com foreign key e customização do admin.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+admin.tittle = "Gerenciamento de Agenda"
+
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento', 'data_criacao')
+    list_filter = ('titulo', 'data_evento', )
+
+
+admin.site.register(Evento, AgendaAdmin)
